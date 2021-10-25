084d87ecacd00a2a6281cdbf6737c60f68db7cd5
Igor Marcos Riegel <igor.riegel@gmail.com>
2021-10-25 18:40:06 -0300

    Implementado script de seed de produtos

--- back/seed.js
+++ back/seed.js
@@ -1,3 +1,24 @@
-const db = require('./models/connection');
+// implementado código de inserção de produtos dummy no banco cfe
+// https://www.w3schools.com/nodejs/nodejs_mongodb_insert.asp
 
-db.products.insertOne({ prod: 'prod teste' });
+const { MongoClient } = require('mongodb');
+
+const url = 'mongodb://127.0.0.1:27017';
+
+MongoClient.connect(url, (err, db) => {
+  if (err) throw err;
+  const dbo = db.db('leilaoSocket');
+
+  const seedArray = [
+    { name: 'produto teste1', value: '0' },
+    { name: 'produto teste2', value: '0' },
+    { name: 'produto teste3', value: '0' },
+    { name: 'produto teste4', value: '0' },
+  ];
+
+  dbo.collection('products').insertMany(seedArray, (error, res) => {
+    if (error) throw error;
+    console.log(`inserted ${seedArray}`);
+    db.close();
+  });
+});
